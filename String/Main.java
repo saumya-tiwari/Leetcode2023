ff67df3211aac0ab84db1bed1cf1991fb5aa6e47
Saumya Tiwari <taru.saumya@gmail.com>
2023-04-17 10:07:20 +0530

    refactored structure

---
+++ String/Main.java
@@ -0,0 +1,28 @@
+package String;
+import java.util.Arrays;
+import java.util.*;
+
+class Main{
+    public static void main(String[] args) {
+        String path = "/./";
+
+        var dirs = new ArrayList<String>();
+        
+        for(var dir :  path.split("/")){
+            if(dir.isEmpty() || dir.equals(".")){
+                continue;
+            }
+
+            if(dir.equals("..")){
+                if(!dirs.isEmpty()){
+                    dirs.remove(dirs.size() - 1);
+                }
+            }else{
+                dirs.add(dir);
+            }
+        }
+
+        System.out.println("/" + String.join("/",dirs));
+
+    }
+}
